12f3bcdf43e0edc29c99493c32d003f0f31c285d
BINODNAYAK806 <binodnayak8066@gmail.com>
2022-09-11 00:51:40 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div style={{background:"#007bff" ,width:"727px", height:"40px" ,padding:"2px"  ,justifyContent: "flexEnd" }}>
+
+        <button style={{background:"#007bff" , border: "1px solid #007bff",padding:"1px" ,margin:"2px"  }}>login</button>
+        <button style={{background:"#007bff" , border: "1px solid #007bff",padding:"1px" ,margin:"2px"  }}>signup</button>
+        
+
+
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import { createStore } from "redux";
 import RootReducer from "./redux/reducers/index";
 import TodoList from "./components/TodoList";
 import TodoInput from "./components/TodoInput";
+import { Navbar } from "./components/Navbar";
 
 import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+// import Navbar from "./components/Navbar";
 const store = createStore(RootReducer);
 
 function App() {
@@ -18,8 +19,10 @@ function App() {
         <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-4">
             {/* <h1 className="text-capitalize text-center">Kelvin Todo List</h1> */}
+            <Navbar/>
             <TodoInput />
             <TodoList />
+
           </div>
         </div>
       </div>
